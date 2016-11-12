9fdd7b55aebab523e1ab4101d2554faf5469993c
szhou1 <zhousteve3@gmail.com>
2016-11-11 16:39:28 -0800

    Connected web terminal with local docker container

--- app/components/Terminal.jsx
+++ app/components/Terminal.jsx
@@ -1,45 +1,49 @@
 const React = require('react');
-// const jQuery = require('jQuery');
 const jQueryTerminal = require('jquery.terminal');
+const axios = require('axios');
 
-class CodeEditor extends React.Component {
+class Terminal extends React.Component {
 	constructor(props) {
 		super(props);
     var context = this;
 		this.state = {
 			command: null
 		}
-    console.log(':-)');
     this.renderTerminal();
 	}
 
-	sendCommand(e) {
-
-	}
-
   renderTerminal() {
-    console.log('render terminal');
-    // console.log(jQuery);
-    // console.log(jQuery.terminal);
+    // console.log('render terminal');
     console.log($);
     console.log($.terminal);
-    // console.log(jQueryTerminal);
 
     $(function($, undefined) {
       $('#terminal').terminal(function(command, term) {
         console.log('command', command);
-          if (command !== '') {
-              var result = window.eval(command);
-              console.log('result', result);
-              if (result != undefined) {
-                  term.echo(String(result));
-              }
-          }
+        if (command !== '') {
+
+          axios.post('/cmd', { cmd: command })
+            .then(function(res) {
+              console.log(res);
+              console.log(res.data);
+              term.echo(String(res.data));
+            })
+            .catch(function(err) {
+              console.error(err);
+              term.echo(String(err));
+            })
+
+            // var result = window.eval(command);
+            // console.log('result', result);
+            // if (result != undefined) {
+            //     term.echo(String(result));
+            // }
+        }
       }, {
-          greetings: 'Javascript Interpreter',
-          name: 'js_demo',
-          height: 300,
-          width: 450,
+          greetings: '',
+          name: '',
+          height: 600,
+          width: 650,
           prompt: '>> '
       });
     });
@@ -47,15 +51,12 @@ class CodeEditor extends React.Component {
 
 	render() {
 		return (
-
       <div>
         <link href="https://cdnjs.cloudflare.com/ajax/libs/jquery.terminal/0.11.13/css/jquery.terminal.min.css" rel="stylesheet"></link>
-
         <div id="terminal"></div><br/>
-        
 			</div>
 		)
 	}
 }
 
-module.exports = CodeEditor;
+module.exports = Terminal;
