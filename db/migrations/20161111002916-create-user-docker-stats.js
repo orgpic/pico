d3835827e98e615b6b3deb0236a4a60c5b0153ec
Steve Zhou <zhousteve3@gmail.com>
2016-11-16 14:35:55 -0800

    Update database table names to lowercase, update db config for test env (#59)
    * Add Dockerfile for picoShell web app, add docker compose file
    
    * Update docker compose file
    
    * Remove web-terminal module
    
    * Update docker files
    
    * Update db config files
    
    * Add use strict to fix weird error on linux
    
    * Lowercase all DB tables to make it work on linux server
    
    * Update db config for test environment, update docker compose

--- db/migrations/20161111002916-create-user-docker-stats.js
+++ db/migrations/20161111002916-create-user-docker-stats.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: function(queryInterface, Sequelize) {
-    return queryInterface.createTable('UserDockerStats', {
+    return queryInterface.createTable('userdockerstats', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -25,6 +25,6 @@ module.exports = {
     });
   },
   down: function(queryInterface, Sequelize) {
-    return queryInterface.dropTable('UserDockerStats');
+    return queryInterface.dropTable('userdockerstats');
   }
 };
