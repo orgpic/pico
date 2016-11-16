f6da433e03f7aec80bd1a5e5ed6d04756d53ba1b
szhou1 <zhousteve3@gmail.com>
2016-11-15 19:02:03 -0800

    Update db config files

--- db/config.js
+++ db/config.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('picoDB', 'root', 'root', {
+const db = new Sequelize('picodb', 'root', 'root', {
   host: 'localhost',
   dialect: 'mysql',
 
