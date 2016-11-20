653d0cd3c656439872b01dd531246e6c7df1ef50
jeff christian <jchri@MacBook-Pro-2.loca>
2016-11-19 17:00:28 -0800

    fix test auth

--- test/routes/auth.spec.js
+++ test/routes/auth.spec.js
@@ -1,33 +1,12 @@
-<<<<<<< 62fc20563be8082c65ab75f2545471af3e1eb0ae
-<<<<<<< 3d9ac1afec2b56c47ce8b5ff1d9e0a3da61168d9
-=======
->>>>>>> rebase
 const should = require("should");
 import { expect } from 'chai';
 import Sequelize from 'sequelize';
 import User from '../../models/User.js';
 const express = require('express');
 const router = express.Router();
-<<<<<<< 62fc20563be8082c65ab75f2545471af3e1eb0ae
 // const port = process.env.PORT || 3000;
 const host = 'http://picoshell.com/';
 const request = require('request');
-=======
-// const should = require("should");
-// import { expect } from 'chai';
-// import Sequelize from 'sequelize';
-// import User from '../../models/User.js';
-// const express = require('express');
-// const router = express.Router();
-// const port = process.env.PORT || 3000;
-// const host = `http://localhost:${port}`;
-// const request = require('request');
->>>>>>> comment out test;
-=======
-// const port = process.env.PORT || 3000;
-const host = 'http://picoshell.com/';
-const request = require('request');
->>>>>>> rebase
 
 describe('Local Signup/Login test', function () {
   const requestWithSession = request.defaults({jar: true});
