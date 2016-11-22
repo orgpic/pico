d1a69b42ce6d8211dbcbb219ea84ddb46a5c8cbb
bianca <bianca.subion@gmail.com>
2016-11-22 11:16:03 -0800

    Show file directory

--- app/components/FileBrowser.jsx
+++ app/components/FileBrowser.jsx
@@ -4,6 +4,7 @@ const axios = require('axios');
 class FileBrowser extends React.Component {
   constructor(props) {
     super(props);
+    console.log('this is props of file browser', props);
     this.socket = io();
     this.state = {
       containerName: this.props.containerName,
@@ -12,6 +13,72 @@ class FileBrowser extends React.Component {
     };
     this.doubleClick = this.doubleClick.bind(this);
   }
+
+  componentWillMount() {
+    const context = this;
+    this.setState({
+      containerName: this.state.containerName
+    });
+    axios.post('/docker/cmd', { cmd: 'ls -al', containerName: context.state.containerName })
+    .then(function(res) {
+      const contents = res.data.split('\n');
+
+      let contentsArr = [];
+
+      for (var i = 0; i < contents.length; i++) {
+        const arr = contents[i].split(" ");
+        let type;
+        if (arr[0].substring(0, 1) === "d") {
+          type = "folder"
+        } else if (arr[0].substring(0, 2) === "-r") {
+          type = "file"
+        }
+        const name = arr[arr.length - 1];
+        contentsArr.push({type: type, name: name});
+      }
+      //remove the last element, which is ''
+      //remove the first element, which is '.'
+      contentsArr.shift();
+      contentsArr.pop();
+      context.setState({
+        contents: contentsArr
+      });
+    });
+  }
+
+
+  componentWillReceiveProps(nextProps) {
+    const context = this;
+    this.setState({
+      containerName: nextProps.containerName
+    });
+    axios.post('/docker/cmd', { cmd: 'ls -al', containerName: nextProps.containerName })
+    .then(function(res) {
+      const contents = res.data.split('\n');
+
+      let contentsArr = [];
+
+      for (var i = 0; i < contents.length; i++) {
+        const arr = contents[i].split(" ");
+        let type;
+        if (arr[0].substring(0, 1) === "d") {
+          type = "folder"
+        } else if (arr[0].substring(0, 2) === "-r") {
+          type = "file"
+        }
+        const name = arr[arr.length - 1];
+        contentsArr.push({type: type, name: name});
+      }
+      //remove the last element, which is ''
+      //remove the first element, which is '.'
+      contentsArr.shift();
+      contentsArr.pop();
+      context.setState({
+        contents: contentsArr
+      });
+    });
+  }
+
 
   doubleClick(e, entry) {
     const context = this;
@@ -27,30 +94,65 @@ class FileBrowser extends React.Component {
             sliced = '/';
           }
           console.log('sliced', sliced);
-          axios.post('/docker/cmd', { cmd: 'ls ' + sliced + ' -a', containerName: context.state.containerName })
+          axios.post('/docker/cmd', { cmd: 'ls ' + sliced + ' -al', containerName: context.state.containerName })
           .then(function(res) {
             const contents = res.data.split('\n');
+            console.log(contents);
+
+            let contentsArr = [];
+
+            for (var i = 0; i < contents.length; i++) {
+              const arr = contents[i].split(" ");
+              let type;
+              if (arr[0].substring(0, 1) === "d") {
+                type = "folder"
+              } else if (arr[0].substring(0, 2) === "-r") {
+                type = "file"
+              }
+              const name = arr[arr.length - 1];
+              contentsArr.push({type: type, name: name});
+            }
             //remove the last element, which is ''
             //remove the first element, which is '.'
-            contents.shift();
-            contents.pop();
+            contentsArr.shift();
+            contentsArr.pop();
+
+            
             context.setState({
-              contents: contents,
+              contents: contentsArr,
               curDir: sliced
             });
           });
         } else {
           var newDir = context.state.curDir + '/' + resp.data.newDir;
           if(newDir.startsWith('//')) newDir = newDir.slice(1);
-          axios.post('/docker/cmd', { cmd: 'ls ' + newDir + ' -a', containerName: context.state.containerName })
+          axios.post('/docker/cmd', { cmd: 'ls ' + newDir + ' -al', containerName: context.state.containerName })
           .then(function(res) {
+            console.log(res.data);
             const contents = res.data.split('\n');
+
+
+            let contentsArr = [];
+
+            for (var i = 0; i < contents.length; i++) {
+              const arr = contents[i].split(" ");
+              let type;
+              if (arr[0].substring(0, 1) === "d") {
+                type = "folder"
+              } else if (arr[0].substring(0, 2) === "-r") {
+                type = "file"
+              }
+              const name = arr[arr.length - 1];
+              contentsArr.push({type: type, name: name});
+            }
+
+            console.log(contentsArr);
             //remove the last element, which is ''
             //remove the first element, which is '.'
-            contents.shift();
-            contents.pop();
+            contentsArr.shift();
+            contentsArr.pop();
             context.setState({
-              contents: contents,
+              contents: contentsArr,
               curDir: newDir
             });
           });
@@ -63,32 +165,24 @@ class FileBrowser extends React.Component {
     });
   }
 
-  componentWillReceiveProps(nextProps) {
-    const context = this;
-    this.setState({
-      containerName: nextProps.containerName
-    });
-    axios.post('/docker/cmd', { cmd: 'ls -a', containerName: nextProps.containerName })
-    .then(function(res) {
-      const contents = res.data.split('\n');
-      //remove the last element, which is ''
-      //remove the first element, which is '.'
-      contents.shift();
-      contents.pop();
-      context.setState({
-        contents: contents
-      });
-    });
-  }
+
 
   render() {
     const context = this;
     return (
         <div>
           {this.state.contents.map(function(entry) {
-            return (
-                <div className="fileBrowser" onDoubleClick={(e) => {context.doubleClick(e, entry)}}>{entry}</div>
-              );
+            if (entry.type === "file") {
+              return (
+                <div className="fileBrowser" onDoubleClick={(e) => {context.doubleClick(e, entry.name)}}><i className="ion-ios-paper-outline">{" " + entry.name}</i></div>
+              )
+            } else if (entry.type === "folder" && entry.name !== ".") {
+              return (
+                <div id={entry.name} className="fileBrowser" onDoubleClick={(e) => {context.doubleClick(e, entry.name)}}>
+                  { entry.name === ".." ? <i className="ion-ios-arrow-up">{entry.name}</i> : <i className="ion-ios-arrow-down">{" " + entry.name}</i> }
+                </div>
+              )
+            }
           })}
         </div>
       );
