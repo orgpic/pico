6ad5f24f5df010f5b61446e7ae698636e1b0f932
bianca <bianca.subion@gmail.com>
2016-11-28 19:16:25 -0800

    fix landing page

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -164,10 +164,60 @@ class App extends React.Component {
                   </div>
                 </div>
               </div>
-              <div className="info-container">
-                Add videos from YouTube to picoShell and instantly code alongside of it
+              <div className="info">
+                <div className="info-header">Why picoShell?</div>
+                <div className="sub-header">
+                  picoShell is for educators, students, developers, and interviewers who want to collaborate through code remotely
+                </div>
+                <div className="info-container">
+                  <img src="/images/header.gif"></img>
+                  <div className="description-left">
+                    <span className="title">Powerful</span><br/>
+                    Gain full access to your own linux terminal emulator and code editor with ruby, node, python, gcc, git, and more installed
+                  </div>
+                </div>
+                <div className="info-container">
+                  <div className="description-right">
+                  <span className="title">Educational</span><br/>
+                  Add videos from YouTube to picoShell and instantly code alongside of it. 
+                  picoShell remembers the file you create with each video, so you can always pick up
+                  where you left off
+                  </div>
+                  <img src="/images/codeable.gif"></img>
+                </div>
+                <div className="info-container">
+                  <img src="/images/header.gif"></img>
+                  <div className="description-left">
+                    <span className="title">Collaborative</span><br/>
+                      Easily give read or write access to your computer to work on projects, assignments,
+                      or to conduct interviews
+                  </div>
+                </div>
+                <div className="info-container">
+                  <div className="description-right">
+                  <span className="title">Convenient</span><br/>
+                    You could just push your code up to git...or you could use picoShell's download button
+                    to export all your files to your local filesystem
+                  </div>
+                  <img src="/images/codeable.gif"></img>
+                </div>
+              </div>
+              <div className="footer">
+                <div className="footer-column">
+                    <p className="footer-header">picoShell</p>
+                    <ul>
+                      <li><img src="/images/whitelogo.svg"></img>About</li>
+                      <li><i className="ion-ios-world-outline"></i>FAQ</li>
+                    </ul>
+                </div>
+                <div className="footer-column">
+                  <p className="footer-header">Contact Us</p>
+                  <ul>
+                    <li><i className="ion-ios-email-outline"></i>bianca.subion@gmail.com</li>
+                  </ul>
               </div>
             </div>
+          </div>
         </div>
       );
     } else {
