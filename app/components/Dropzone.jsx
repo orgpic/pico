7c08ce422b3ed18f04a6f75c7ebf563a588804a1
Lucas Becker <Lucas.D.Becker@gmail.com>
2016-11-30 12:09:22 -0800

    Remove redundant files

--- app/components/Dropzone.jsx
+++ app/components/Dropzone.jsx
@@ -43,51 +43,5 @@ var Dropzone = React.createClass({
       });
   }
 });
-/*
-class Dropzone extends React.Component {
-    constructor(props) {
-        super(props);
-
-        // For a full list of possible configurations,
-        // please consult http://www.dropzonejs.com/#configuration
-        this.djsConfig = {
-            acceptedFiles: "text/plain",
-            addRemoveLinks: true,
-            params: {
-                myParam: 'Hello from a parameter!',
-                anotherParam: 43
-            },
-            autoProcessQueue: true,
-            uploadMultiple: false
-        };
-
-        this.componentConfig = {
-            iconFiletypes: ['.txt'],
-            showFiletypeIcon: true,
-            postUrl: '/uploadHandler'
-        };
-
-        // If you want to attach multiple callbacks, simply
-        // create an array filled with all your callbacks.
-        this.callbackArray = [() => console.log('Hi!'), () => console.log('Ho!')];
-
-        // Simple callbacks work too, of course
-        this.callback = () => console.log('Hello!');
-    }
-
-    render() {
-        const config = this.componentConfig;
-        const djsConfig = this.djsConfig;
-
-        // For a list of all possible events (there are many), see README.md!
-        const eventHandlers = {
-            drop: this.callbackArray,
-            addedfile: this.callback,
-        }
-
-        return <DropzoneComponent config={config} eventHandlers={eventHandlers} djsConfig={djsConfig} />
-    }
-}
-*/
 
 module.exports = Dropzone;
