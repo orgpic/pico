c9faf2a6a96be22e4239e524874e1b54c64435b3
szhou1 <zhousteve3@gmail.com>
2016-11-30 13:59:52 -0800

    (feat) Add description under video search results

--- app/components/VideoSearchResults.jsx
+++ app/components/VideoSearchResults.jsx
@@ -3,17 +3,24 @@ const React = require('react');
 const VideoSearchResults = ({ videos, handleSearchedVideoClick }) => {
 
   // console.log(videos);
-  const getVideoUrls = (videos) => videos.map(video => video.id.videoId );
+  // const getVideoUrls = (videos) => videos.map(video => video.id.videoId );
   // console.log(getVideoUrls(videos));
 
   return (
-    <div className="video-search-image-container">
+    <div className="video-search-image-container row">
       {
         videos.map((video) => {
           // console.log(video.snippet.thumbnails.high.url);
-
-          return <img key={video.id.videoId} src={video.snippet.thumbnails.medium.url} onClick={handleSearchedVideoClick.bind(null, video)} />
-          
+          // console.log(video.snippet.description);
+          return (
+            <div className="video-search-result">
+              <img className="video-search-image"
+                      key={video.id.videoId} 
+                      src={video.snippet.thumbnails.medium.url} 
+                      onClick={handleSearchedVideoClick.bind(null, video)} />
+                  <p className="video-search-description">{video.snippet.description}</p>
+            </div>
+          )
         })
       }
 
