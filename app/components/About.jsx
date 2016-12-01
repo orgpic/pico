bc1e92d122fe93367640d3f5fb91567d686c3227
Lucas Becker <Lucas.D.Becker@gmail.com>
2016-12-01 15:34:51 -0800

    Fill out FAQ answers and my bio section.

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -58,7 +58,11 @@ class About extends React.Component {
                 <div className='teamRow'>
                   <img className='pic' src="/images/LB.jpg"></img>
                   <div className='tText'>
-                    SOMETHING CREATIVE Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi aliquam magna in massa dapibus, eget malesuada dui accumsan. Nullam sodales congue condimentum. Aenean gravida, dolor ut iaculis iaculis, massa tortor aliquet metus, id tincidunt orci orci nec ex. Nunc molestie, ante mollis consectetur porttitor, mi turpis tempus nisl, vel commodo velit tellus quis massa. In semper justo a quam efficitur facilisis. Proin quis risus in sapien pellentesque placerat quis eu risus. Nullam eu pellentesque mauris. Nam varius mollis augue, at commodo mauris dictum sed. Aenean iaculis feugiat molestie. Sed in libero commodo, fermentum leo ut, malesuada eros. Nulla sit amet lectus non lorem tristique volutpat a in nunc.
+                    Lucas began programming at the age of 13, and met the other members of the picoShell Team while at Hack
+                    Reactor in San Francisco after attending Carnegie Mellon University's School of Computer Science
+                     and is a life long New York Yankees and Jets fan. As a member of the picoShell team, he was responsible for 
+                     implementing synchronization between collaborators working on the same computer, the ability to upload
+                     and download files from your computer and the File Browser. 
                   </div>
                 </div>
               </div>
